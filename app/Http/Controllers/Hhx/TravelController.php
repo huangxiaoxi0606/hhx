332b17ce7b2f8ffa82bdb3a73d794f5239703f45
hhx <1007502956@qq.com>
2019-12-27 18:07:08 +0800

    增加旅行前端查看

---
+++ app/Http/Controllers/Hhx/TravelController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hhx
+ * Date: 2019/12/27
+ * Time: 18:03
+ */
+
+namespace App\Http\Controllers\Hhx;
+
+use App\Http\Controllers\Controller;
+use App\Models\HhxTravel;
+use Illuminate\Support\Facades\Request;
+
+class TravelController extends Controller
+{
+    public function getList(Request $request)
+    {
+        $datas = HhxTravel::query()->paginate(20);
+        $data['data'] = $datas;
+        return view('Home.Hhx.travel_list', $data);
+    }
+
+    public function getIntro(Request $request)
+    {
+        $id = $request->get('id');
+        $datas = HhxTravel::whereId($id)->first();
+        $data['data'] = $datas;
+        return view('Home.Hhx.travel_intro', $data);
+    }
+
+}
