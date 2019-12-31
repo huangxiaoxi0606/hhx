18ba4a8a79f5c6a7b3ee75b66dca0b6b7f3362d4
hhx <1007502956@qq.com>
2019-12-31 17:01:50 +0800

    direction增加年总和

--- app/Models/Direction.php
+++ app/Models/Direction.php
@@ -6,9 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Direction extends Model
 {
-    //
+    protected $appends = [
+        'this_year',
+    ];
+
     public static function getData()
     {
         return Direction::whereStatus(0)->pluck('name', 'id');
+    }
+
+    public function getThisYearAttribute()
+    {
+        return DirectionLog::where('direction_id', $this->id)->where('created_at', '>', '2020-01-01')->sum('money');
     }
 }
